9e5e470442afd5a09ebc9d716774d730c3c9a588
Anurag Regmi <anuragregmi@protonmail.com>
2021-04-12 00:33:02 +0545

    Implement basic chat feature

--- src/components/chatbox.js
+++ src/components/chatbox.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Peer from 'peerjs'
 
 export default class ChatBox extends React.Component {
     constructor(props) {
         super(props)
-        this.messages = [
+        var messages = [
             {
                 "text": "Hi sweetheart!",
                 "user": {
@@ -19,18 +20,119 @@ export default class ChatBox extends React.Component {
                 "self": false
             }
         ]
+        this.peer = null
+        this.connection = null
+        this.state = {
+            peerId: 'N/A',
+            messages: messages
+        }
+
+        // bind event listeners
+        this.onKeyPressPeerBox = this.onKeyPressPeerBox.bind(this)
+        this.onKeyPressSendMessageBox = this.onKeyPressSendMessageBox.bind(this)
+        this.receiveConnection = this.receiveConnection.bind(this)
+        this.receiveMessage = this.receiveMessage.bind(this)
     }
+
+    onKeyPressPeerBox(e) {
+        var code = (e.keyCode ? e.keyCode : e.which);
+        if (code === 13) {
+            e.preventDefault();
+            if (!this.state.peerId | this.state.peerId === 'N/A') {
+                alert("Not connected to server yet. Please wait.")
+                return
+            }
+            this.connectPeer(e.target.value)
+        }
+
+    }
+
+    onKeyPressSendMessageBox(e) {
+        var code = (e.keyCode ? e.keyCode : e.which);
+        if (code === 13) {
+            e.preventDefault();
+            if (!(this.connection && this.connection.peer)) {
+                alert("Not connected to peer yet. Can not send message.")
+                return
+            }
+            this.sendMessage(e.target.value)
+            e.target.value = ""
+        }
+    }
+
+    setConnectedId(id) {
+        document.getElementById("peer-input").value = id
+    }
+
+    sendMessage(text) {
+        var message = {
+            "text": text,
+            "user": {
+                "profile_picture": "https://source.unsplash.com/vpOeXr5wmR4/600x600",
+            },
+            "self": true
+        }
+        this.connection.send(text)
+        this.setState({ 'messages': this.state.messages.concat([message]) })
+        this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+
+    }
+
+    receiveMessage(text) {
+        var message = {
+            "text": text,
+            "user": {
+                "profile_picture": "https://source.unsplash.com/otT2199XwI8/600x600",
+            },
+            "self": false
+        }
+        this.setState({ 'messages': this.state.messages.concat([message]) })
+        this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+    }
+
+    connectPeer(peerId) {
+        var conn = this.peer.connect(peerId)
+        conn.on('open', () => {
+            this.connection = conn
+            conn.on('data', this.receiveMessage)
+        })
+    }
+    receiveConnection(conn) {
+        this.setConnectedId(conn.peer)
+        this.connection = conn
+        conn.on('data', this.receiveMessage)
+    }
+
+    componentDidMount() {
+        this.peer = new Peer()
+        this.peer.on('open', (id) => { this.setState({ peerId: id }) })
+        this.peer.on('connection', this.receiveConnection)
+    }
+
     render() {
         return (
-            <div className="p-4 m-5 shadow-lg bg-white w-full max-w-screen-md h-full overflow-y-auto">
+            <div className="p-4 m-5 shadow-lg bg-white w-full max-w-screen-md h-full">
                 <div className="h-1/6">
-                    <div className="font-semibold text-2xl border-b pb-2">Let's Chat</div>
+                    <div className="border-b pb-2">
+                        <div className="flex">
+                            <div className="flex w-1/6">
+                                <span className="font-semibold flex-col text-2xl">Let's Chat</span>
+                            </div>
+                            <div className="flex w-5/6 justify-end">
+                                <span className="text-gray-400 italic py-1">Your Id: {this.state.peerId}</span>
+                                <span className="text-gray-400 italic py-1 ml-3">Connected to:</span>
+                                <input className="ml-1 p-1 border" placeholder="Peer's Id" id="peer-input"
+                                    onKeyPress={this.onKeyPressPeerBox}></input>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
-                <div className="w-full px-5 flex flex-col justify-between h-4/6">
+                <div className="w-full px-5 flex flex-col justify-between h-4/6  overflow-y-auto">
 
                     <div className="flex w-full flex-col mt-5">
                         {
-                            this.messages.map((message, i) =>
+                            this.state.messages.map((message, i) =>
                                 message.self === true
                                     ? <div className="flex justify-end mb-4" key={i}>
 
@@ -61,7 +163,9 @@ export default class ChatBox extends React.Component {
                                     </div>
                             )
                         }
-
+                        <div style={{ float: "left", clear: "both" }}
+                            ref={(el) => { this.messagesEnd = el; }}>
+                        </div>
                     </div>
                 </div>
                 <div className="w-full px-5 flex flex-col justify-between h-1/6">
@@ -70,6 +174,7 @@ export default class ChatBox extends React.Component {
                             className="w-5/6 bg-gray-300 py-5 px-3 rounded-xl"
                             type="text"
                             placeholder="type your message here..."
+                            onKeyPress={this.onKeyPressSendMessageBox}
                         />
                         <svg xmlns="http://www.w3.org/2000/svg"
                             className="inline-block h-10 w-10 transform rotate-90 hover:text-blue-600"
